1976a7f52b44ff188774b30ffc598da0f93b7b2f
EC2 Default User <violet794.py16@gmail.com>
2021-04-29 13:56:02 +0000

    [finish]4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -12,14 +14,25 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
 
   def edit
+    @blog = Blog.find(params[:id])
   end
 
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog.id)
+  end
 
+   def destroy
+     blog = Blog.find(params[:id])
+     blog.destroy
+     redirect_to blogs_path
+   end
 
 
 
@@ -27,4 +40,5 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
+  
 end
