36348a11f90d941274af649b38a1c68c1670ada9
EC2 Default User <violet794.py16@gmail.com>
2021-04-29 11:14:09 +0000

    [add]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,11 +3,28 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.new
   end
 
   def new
   end
 
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+
+
   def edit
+  end
+
+
+
+
+
+  private
+  def blog_params
+    params.permit(:title, :category, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   resources :blogs
 
+  post 'blogs/new' => 'blogs#create'
+
 end
